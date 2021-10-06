ea1d59990bc6d72527dd2391183e193e3f47e4be
Natalia Jurecka <jurecka.natalia22@gmail.com>
2021-10-06 16:28:34 +0200

    1a done, hope so

--- portfoliolab_project/src/App.js
+++ portfoliolab_project/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from "./comonents/Home/Home";
+import Form from "./comonents/Form";
+import Login from "./comonents/Login";
+import Register from "./comonents/Register";
+import Logout from "./comonents/Logout";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
-      </header>
+        <Router>
+            <Switch>
+                <Route path='/' exact component={Home}/>
+                <Route path='/oddaj-rzeczy' component={Form}/>
+                <Route path='/logowanie' component={Login}/>
+                <Route path='/rejestracja' component={Register}/>
+                <Route path='/wylogowano' component={Logout}/>
+            </Switch>
+        </Router>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ portfoliolab_project/src/comonents/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './App.css';
+
+function Form() {
+    return (
+        <div>
+            <h1>Form</h1>
+        </div>
+    );
+}
+
+export default Form;

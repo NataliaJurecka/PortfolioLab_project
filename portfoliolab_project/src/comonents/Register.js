65f6fbe9ac961e3e249abff5644dad6eea958188
Natalia Jurecka <jurecka.natalia22@gmail.com>
2021-10-13 23:13:00 +0200

    3a done

--- portfoliolab_project/src/comonents/Register.js
+++ portfoliolab_project/src/comonents/Register.js
@@ -1,10 +1,30 @@
 import React from 'react';
-// import './App.css';
+import decorUrl from "../assets/Decoration.svg";
+import {Link} from "react-router-dom";
+
 
 function Register() {
     return (
         <div>
-            <h1>Register</h1>
+            <div className='registration_view'>
+                <h1>Załóż konto</h1>
+                <img className='decor' src={decorUrl} alt=''/>
+                <div className='registration_form'>
+                    <label>E-mail
+                        <input type='email'/>
+                    </label>
+                    <label>Hasło
+                        <input type='password'/>
+                    </label>
+                    <label>Powtórz hasło
+                        <input type='password'/>
+                    </label>
+                </div>
+                <div className='btn-container'>
+                    <button className='btn_3'>Załóż konto</button>
+                <Link to='/logowanie'><button className='btn_3'>Zaloguj się</button></Link>
+                </div>
+            </div>
         </div>
     );
 }

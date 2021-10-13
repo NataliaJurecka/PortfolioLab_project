7ff09b31c981bf30d3f91ae81d2b6a93638802e0
Natalia Jurecka <jurecka.natalia22@gmail.com>
2021-10-13 23:20:55 +0200

    4a done

--- portfoliolab_project/src/comonents/Logout.js
+++ portfoliolab_project/src/comonents/Logout.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import decorUrl from "../assets/Decoration.svg";
+import {Link} from "react-router-dom";
 // import './App.css';
 
 function Logout() {
     return (
-        <div>
-            <h1>Logout</h1>
+        <div className='logout_view'>
+            <h1>Wylogowanie nastąpiło pomyślnie!</h1>
+            <img className='decor' src={decorUrl} alt=''/>
+            <Link to='/'><button className='btn_3'>Strona główna</button></Link>
         </div>
     );
 }
